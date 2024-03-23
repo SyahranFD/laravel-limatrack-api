85240572067acdda55970d7abf0d95a5b1a04f5b
SyahranFD <fadhilrafa1@gmail.com>
2024-03-23 20:16:01 +0700

    add factory user

--- database/factories/UserCustomerFactory.php
+++ database/factories/UserCustomerFactory.php
@@ -3,13 +3,11 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
+class UserCustomerFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -22,23 +20,12 @@ class UserFactory extends Factory
             'id' => fake()->uuid(),
             'nama_lengkap' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'password' => fake()->password(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role' => 'customer',
             'verified_email' => 1,
             'profile_picture' => null,
             'latitude' => null,
             'longitude' => null,
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return $this
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
